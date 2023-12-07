8adbba589872df788736f861f6872ff9ac3baf29
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2023-12-07 05:59:20 -0300

    Cors!

--- server/src/main/java/ttps/java/entregable6_v2/Entregable6Application.java
+++ server/src/main/java/ttps/java/entregable6_v2/Entregable6Application.java
@@ -3,6 +3,9 @@ package ttps.java.entregable6_v2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class}, scanBasePackages = {"ttps.java.entregable6_v2"})
--- server/src/main/java/ttps/java/entregable6_v2/config/SecurityConfig.java
+++ server/src/main/java/ttps/java/entregable6_v2/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ttps.java.entregable6_v2.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,9 +12,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import ttps.java.entregable6_v2.filter.JwtAuthenticationFilter;
 
 import java.security.AuthProvider;
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,19 +28,37 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     private final AuthenticationProvider authProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.
-                csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest ->
-                        authRequest.
-                                requestMatchers("/auth/**").permitAll().
-                                anyRequest().authenticated()
-                ).sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS)
-                ).authenticationProvider(authProvider)
+                        authRequest
+                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .cors(cors -> {
+                    cors.configurationSource(request -> {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Arrays.asList("https://localhost:4200"));
+                        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+                        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+                        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+                        corsConfiguration.setAllowCredentials(true);
+                        return corsConfiguration;
+                    });
+                });
+
+        return http.build();
     }
+
+
 }
