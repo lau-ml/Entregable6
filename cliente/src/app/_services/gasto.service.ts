8eae41c0d5b70f635eb69f1d3e59bc7cbf259384
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-01-18 16:34:06 -0300

    Gasto service

---
+++ cliente/src/app/_services/gasto.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GastoService {
+  private perPage: number = 10;
+
+  constructor(private httpClient: HttpClient,
+  ) {
+  }
+
+  crearGasto(gasto: any) {
+
+
+    return this.httpClient.post("http://localhost:8080/gasto/crear", gasto);
+  }
+
+  getPerPages() {
+    return this.perPage;
+  }
+
+  getGastos(page: number) {
+
+    const url = `http://localhost:8080/gasto/todos?page=${page}&pageSize=${this.perPage}`;
+
+    return this.httpClient.get<any>(url).pipe(
+      map((grupoData) => {
+          return {
+            gastos: grupoData.gastos,
+            totalItems: grupoData.totalItems,
+            totalPages: grupoData.totalPages,
+            currentPage: grupoData.currentPage
+          };
+        }
+      ));
+  }
+
+
+}
