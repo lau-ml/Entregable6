7b15f6598ce6921a6fd01220e0016cb7ef578dc4
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-02-15 13:47:10 -0300

    Paso algunas cosas del listado de gastos a la creación

--- cliente/src/app/gasto/gasto-creacion/gasto-creacion.component.ts
+++ cliente/src/app/gasto/gasto-creacion/gasto-creacion.component.ts
@@ -1,12 +1,144 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {GastoResponse} from "../../_responses/gastoResponse";
+import {GastoService} from "../../_services/gasto.service";
+import {UsuarioService} from "../../_services/usuario.service";
 
 @Component({
   selector: 'app-gasto-creacion',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule
+  ],
   templateUrl: './gasto-creacion.component.html',
   styleUrl: './gasto-creacion.component.css'
 })
 export class GastoCreacionComponent {
+  formulario: FormGroup = new FormGroup({});
+  imagen: File = new File([], "");
+  private id: number = 0;
+  gastos: GastoResponse[] = [];
+  totalItems: number = 0;
+  totalPages: number = 0;
+  currentPage: number = 0;
+  loading: boolean = true;
+  itemsPerPage: number = 0;
 
+  constructor(private formBuilder: FormBuilder, private gastoService: GastoService,
+              private usuarioService: UsuarioService) {
+  }
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      monto: ['', Validators.required],
+      fecha: ['', Validators.required],
+      imagen: ['', Validators.required],
+      grupoBool: [false],
+      id_grupo: ['', Validators.required],
+      division: ['', Validators.required],
+      tipo: ['', Validators.required],
+      personas: this.formBuilder.array([]),
+    });
+    this.usuarioService.getUsuario().subscribe({
+      next: (data) => {
+        this.id = data.id;
+      }
+    })
+  }
+
+
+  get personasFormArray() {
+    return this.formulario.get('personas') as FormArray;
+  }
+
+  agregarUsuario() {
+    this.personasFormArray.push(this.formBuilder.group({
+      nombre: ['', Validators.required],
+      monto: ['', Validators.required],
+    }));
+  }
+
+
+
+  eliminarUsuario(index: number) {
+    this.personasFormArray.removeAt(index);
+  }
+
+  guardarRecibo() {
+
+    const formData = new FormData();
+    this.armarGastoRequest(this.formulario);
+    const blob = new Blob([JSON.stringify(this.formulario.value)], {
+      type: 'application/json'
+    });
+    formData.append('imagen', this.imagen);
+    formData.append('gastoRequest', blob);
+    // Realiza la solicitud HTTP POST
+    this.gastoService.crearGasto(formData).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.resetearForm();
+        },
+        error: (error) => {
+          console.log(error);
+        }
+        ,
+        complete: () => {
+          this.formulario.reset();
+        }
+      }
+    );
+  }
+
+
+  armarGastoRequest(formulario: FormGroup) {
+    if (!formulario.value.grupoBool) {
+      formulario.value.id_grupo = 0;
+    }
+    if (formulario.value.division === 'MONTOIGUAL') {
+      formulario.value.personas.forEach((persona: any) => {
+        persona.monto = formulario.value.monto / formulario.value.personas.length;
+      });
+      formulario.value.division = 'MONTO';
+    }
+    if (formulario.value.division === 'PORCENTAJEIGUAL') {
+      formulario.value.personas.forEach((persona: any) => {
+        persona.monto = (formulario.value.monto / formulario.value.personas.length) * 100 / formulario.value.monto;
+      });
+      formulario.value.division = 'PORCENTAJE';
+    }
+    let suma: number = 0;
+    formulario.value.personas.forEach((persona: any) => {
+        suma += Number(persona.monto);
+
+      }
+    );
+    if (formulario.value.division === 'MONTO') {
+      formulario.value.personas.push({id: this.id, monto: formulario.value.monto - suma});
+    } else {
+      formulario.value.personas.push({id: this.id, monto: 100 - suma});
+    }
+
+
+  }
+
+  resetearForm() {
+    this.formulario.reset();
+    this.personasFormArray.clear();
+  }
+
+  onChange(event: any) {
+
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.imagen = file;
+    } else {
+      this.imagen = new File([], "");
+    }
+  }
 }
--- cliente/src/app/gasto/gastos/gastos.component.ts
+++ cliente/src/app/gasto/gastos/gastos.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 import {GastoService} from "../../_services/gasto.service";
 import {UsuarioService} from "../../_services/usuario.service";
 import {GastoResponse} from "../../_responses/gastoResponse";
@@ -10,8 +10,7 @@ import {GastoResponse} from "../../_responses/gastoResponse";
   styleUrl: './gastos.component.css'
 })
 export class GastosComponent {
-  formulario: FormGroup = new FormGroup({});
-  imagen: File = new File([], "");
+
   private id: number = 0;
   gastos: GastoResponse[] = [];
   totalItems: number = 0;
@@ -25,34 +24,13 @@ export class GastosComponent {
   }
 
   ngOnInit() {
-    this.formulario = this.formBuilder.group({
-      monto: ['', Validators.required],
-      fecha: ['', Validators.required],
-      imagen: ['', Validators.required],
-      grupoBool: [false],
-      id_grupo: ['', Validators.required],
-      division: ['', Validators.required],
-      tipo: ['', Validators.required],
-      personas: this.formBuilder.array([]),
-    });
+
     this.usuarioService.getUsuario().subscribe({
       next: (data) => {
         this.id = data.id;
       }
     })
     this.getPage(1);
-  }
-
-
-  get personasFormArray() {
-    return this.formulario.get('personas') as FormArray;
-  }
-
-  agregarUsuario() {
-    this.personasFormArray.push(this.formBuilder.group({
-      nombre: ['', Validators.required],
-      monto: ['', Validators.required],
-    }));
   }
 
   getPage(page: number) {
@@ -73,85 +51,5 @@ export class GastosComponent {
         }
       }
     )
-  }
-
-  eliminarUsuario(index: number) {
-    this.personasFormArray.removeAt(index);
-  }
-
-  guardarRecibo() {
-
-    const formData = new FormData();
-    this.armarGastoRequest(this.formulario);
-    const blob = new Blob([JSON.stringify(this.formulario.value)], {
-      type: 'application/json'
-    });
-    formData.append('imagen', this.imagen);
-    formData.append('gastoRequest', blob);
-    // Realiza la solicitud HTTP POST
-    this.gastoService.crearGasto(formData).subscribe(
-      {
-        next: (data) => {
-          console.log(data);
-          this.resetearForm();
-        },
-        error: (error) => {
-          console.log(error);
-        }
-        ,
-        complete: () => {
-          this.formulario.reset();
-          this.getPage(this.currentPage);
-        }
-      }
-    );
-  }
-
-
-  armarGastoRequest(formulario: FormGroup) {
-    if (!formulario.value.grupoBool) {
-      formulario.value.id_grupo = 0;
-    }
-    if (formulario.value.division === 'MONTOIGUAL') {
-      formulario.value.personas.forEach((persona: any) => {
-        persona.monto = formulario.value.monto / formulario.value.personas.length;
-      });
-      formulario.value.division = 'MONTO';
-    }
-    if (formulario.value.division === 'PORCENTAJEIGUAL') {
-      formulario.value.personas.forEach((persona: any) => {
-        persona.monto = (formulario.value.monto / formulario.value.personas.length) * 100 / formulario.value.monto;
-      });
-      formulario.value.division = 'PORCENTAJE';
-    }
-    let suma: number = 0;
-    formulario.value.personas.forEach((persona: any) => {
-        suma += Number(persona.monto);
-
-      }
-    );
-    if (formulario.value.division === 'MONTO') {
-      formulario.value.personas.push({id: this.id, monto: formulario.value.monto - suma});
-    } else {
-      formulario.value.personas.push({id: this.id, monto: 100 - suma});
-    }
-
-
-  }
-
-  resetearForm() {
-    this.formulario.reset();
-    this.personasFormArray.clear();
-  }
-
-  onChange(event: any) {
-
-    const file: File = event.target.files[0];
-
-    if (file) {
-      this.imagen = file;
-    } else {
-      this.imagen = new File([], "");
-    }
   }
 }
